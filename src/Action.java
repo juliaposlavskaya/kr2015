8fe4f1bb7ca8da85d03437234f5e4bef5eb3904b
Julia <julia.poslavskaya@mail.ru>
2015-12-16 23:47:10 +0200

    MenuBar
    добавила строчку с меню

--- src/Action.java
+++ src/Action.java
@@ -42,6 +42,33 @@ public class Action extends JFrame {
 
         this.setResizable(false); //запрещаем изменение размера окна
 
+        JMenuBar menuBar = new JMenuBar(); //Создаем основное меню
+
+        //Создаем подменю
+        JMenu menuGame = new JMenu("Игра");
+        JMenu menuHelp = new JMenu("Помощь");
+
+        //Добавляем подменю в основное меню
+        menuBar.add(menuGame);
+        menuBar.add(menuHelp);
+
+        this.setJMenuBar(menuBar); //Устанавливаем полученное меню на окно
+
+        //Создаем элементы подменю Game с обработчиками событий
+        JMenuItem newGame = new JMenuItem("Новая игра");
+        JMenuItem record = new JMenuItem("Рекорды");
+        JMenuItem exit = new JMenuItem("Выход");
+
+        //Создаем элемент подменю Help с обработчиками событий
+        JMenuItem specification = new JMenuItem("Правила");
+
+        //Добавляем созданные элементы к подменю
+        menuGame.add(newGame);
+        menuGame.add(record);
+        menuGame.add(exit);
+        menuHelp.add(specification);
+
+
         gamePanel.setLayout(null);
 
         // добавляем кнопочки
